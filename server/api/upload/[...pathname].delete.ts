01da87b90f54b7972a574e96f67ba0574d360baa
Hugo <hugo.richard@vercel.com>
2025-12-11 18:08:05 +0100

    Update server/api/upload/[...pathname].delete.ts
    Co-authored-by: Sébastien Chopin <atinux@gmail.com>

--- server/api/upload/[...pathname].delete.ts
+++ server/api/upload/[...pathname].delete.ts
@@ -2,16 +2,8 @@ import { blob } from 'hub:blob'
 import { z } from 'zod'
 
 export default defineEventHandler(async (event) => {
-  const session = await getUserSession(event)
-
-  if (!session.user) {
-    throw createError({
-      statusCode: 401,
-      statusMessage: 'Authentication required to delete files'
-    })
-  }
-
-  const username = session.user.username
+  const { user } = await requireUserSession(event)
+  const { username } = user
 
   const { pathname } = await getValidatedRouterParams(event, z.object({
     pathname: z.string().min(1)
