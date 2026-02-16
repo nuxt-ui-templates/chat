f810385b0db10e7db5c6cf3fef34b39cdc6c1e31
Benjamin Canac <canacb1@gmail.com>
2026-02-16 18:21:16 +0100

    feat(app): update models list

--- app/composables/useModels.ts
+++ app/composables/useModels.ts
@@ -17,7 +17,7 @@ export function useModels() {
   const models = [
     'openai/gpt-5-nano',
     'anthropic/claude-haiku-4.5',
-    'google/gemini-2.5-flash'
+    'google/gemini-3-flash'
   ]
 
   const model = useCookie<string>('model', { default: () => 'openai/gpt-5-nano' })
