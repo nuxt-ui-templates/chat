12881413ec5523763cf70badc3c6fe1a366019f8
Benjamin Canac <canacb1@gmail.com>
2026-09-15 12:32:54 +0200

    chore: add eslint-plugin-better-tailwindcss (#190)

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,28 @@
 // @ts-check
 import withNuxt from './.nuxt/eslint.config.mjs'
+import betterTailwindcss from 'eslint-plugin-better-tailwindcss'
+import { getDefaultAttributes } from 'eslint-plugin-better-tailwindcss/api/defaults'
 
-export default withNuxt({
-  rules: {
-    'vue/multi-word-component-names': 'off',
-    'vue/max-attributes-per-line': ['error', { singleline: 3 }]
+export default withNuxt(
+  {
+    rules: {
+      'vue/multi-word-component-names': 'off',
+      'vue/max-attributes-per-line': ['error', { singleline: 3 }]
+    }
+  },
+  betterTailwindcss.configs['correctness-error'],
+  {
+    settings: {
+      'better-tailwindcss': {
+        entryPoint: 'app/assets/css/main.css',
+        attributes: [
+          ...getDefaultAttributes(),
+          ['^v-bind:ui$', [{ match: 'objectValues' }]]
+        ]
+      }
+    },
+    rules: {
+      'better-tailwindcss/no-unknown-classes': ['error', { ignore: ['^dot-pattern$'] }]
+    }
   }
-})
+)
